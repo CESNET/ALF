fcbb7a849eccb6185d4d3a6905e7951d5c8a4b0c
Jaroslav Pesek <jaroslav.pesek@icloud.com>
2022-07-11 22:56:41 +0200

    gui parameter passing

--- alf_gui.py
+++ alf_gui.py
@@ -4,15 +4,50 @@ import numpy as np
 import uuid
 
 
+DEFAULT_FEATURES = [
+    'bytes_rev',
+    'bytes',
+    'packets',
+    'packets_rev',
+    'packets_sum',
+    'bytes_ration',
+    'num_pkts_ration',
+    'time',
+    'av_pkt_size',
+    'av_pkt_size_rev',
+    'var_pkt_size',
+    'var_pkt_size_rev',
+    'median_pkt_size',
+    'median_pkt_size_rev',
+    'mindelay',
+    'avgdelay',
+    'maxdelay',
+    'bursts',
+    'fizzles',
+    'time_leap_ration',
+    'autocorr',
+    'stSum',
+    'ndSum',
+    'rdSum'
+]
+
+
 st.sidebar.title('Select parameters')
 
 model = st.sidebar.selectbox('Model selection', ['Random Forest', 'Decision Tree'])
+
+nmax = st.sidebar.number_input('Max number of queried flows', min_value=0, value=1)
+
 strategy = st.sidebar.selectbox('Query strategy selection', ['Random', "Uncertainty"])
 
 if strategy == 'Uncertainty':
     threshold = st.sidebar.number_input('Uncertainty threshold', max_value=1., min_value=0., value=0.6)
 
-d0_file = st.sidebar.file_uploader("Choose a CSV file", type="csv")
+d0_file = st.sidebar.file_uploader("Choose a initial trainset file", accept_multiple_files=False, type="csv")
+stream_files = st.sidebar.file_uploader("Choose a stream data", accept_multiple_files=True, type="csv")
+
+features = st.sidebar.text_area("List of features to use, comma separated", value=",".join(DEFAULT_FEATURES))
+
 run_id = st.sidebar.text_input('Run ID', placeholder="If empty, random ID will be used")
 workdir = st.sidebar.text_input('Workdir', placeholder='If empty, /tmp/ will be used')
 
